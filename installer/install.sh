995dc0b1eba354ff331a5a03b49b2479ce4bb883
Eyesonjune18 <lowellashton@gmail.com>
2022-12-24 13:11:48 -0800

    Added downloaders for scripts.

--- installer/install.sh
+++ installer/install.sh
@@ -28,11 +28,11 @@ mkdir ~/.local/bin/ally
 install="$HOME/.local/bin/ally"
 
 echo "Downloading script files to install folder"
-touch "$install/mkal.sh"
-touch "$install/scral.sh"
-touch "$install/real.sh"
-touch "$install/dual.sh"
-touch "$install/rebash.sh"
+curl -LJO https://github.com/Eyesonjune18/Ally/blob/f2c3ec3aac5da88a9ca015717ae7862220546943/src/mkal.sh -o "$install/mkal.sh"
+curl -LJO https://github.com/Eyesonjune18/Ally/blob/f2c3ec3aac5da88a9ca015717ae7862220546943/src/scral.sh -o "$install/scral.sh"
+curl -LJO https://github.com/Eyesonjune18/Ally/blob/f2c3ec3aac5da88a9ca015717ae7862220546943/src/real.sh -o "$install/real.sh"
+curl -LJO https://github.com/Eyesonjune18/Ally/blob/f2c3ec3aac5da88a9ca015717ae7862220546943/src/dual.sh -o "$install/dual.sh"
+curl -LJO https://github.com/Eyesonjune18/Ally/blob/f2c3ec3aac5da88a9ca015717ae7862220546943/src/rebash.sh -o "$install/rebash.sh"
 
 echo "Creating alias configuration files"
 # touch ~/.bash_aliases
