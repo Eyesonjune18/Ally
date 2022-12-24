50bfb4e8f22d592c089fc896416ff041d95d85fc
Eyesonjune18 <lowellashton@gmail.com>
2022-12-24 13:16:33 -0800

    Fixed paths for installed files.

--- installer/install.sh
+++ installer/install.sh
@@ -35,7 +35,7 @@ curl -LJO https://github.com/Eyesonjune18/Ally/blob/f2c3ec3aac5da88a9ca015717ae7
 curl -LJO https://github.com/Eyesonjune18/Ally/blob/f2c3ec3aac5da88a9ca015717ae7862220546943/src/rebash.sh -o "$install/rebash.sh"
 
 echo "Creating alias configuration files"
-# touch ~/.bash_aliases
-# touch ~/.bash_aliases_persistent
+curl -LJO https://github.com/Eyesonjune18/Ally/blob/995dc0b1eba354ff331a5a03b49b2479ce4bb883/installer/.bash_aliases_persistent -o "$HOME/.bash_aliases_persistent"
+cp "$HOME/.bash_aliases_persistent" "$HOME/.bash_aliases"
 
-echo "Ally has been installed successfully."
+echo "Ally has been installed successfully. Please restart your terminal or type \"source ~/.bashrc\" to apply changes."
