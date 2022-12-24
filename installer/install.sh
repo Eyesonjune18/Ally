e0ff09bc34ab89e178dfcbb3ec8098c618028fc0
Eyesonjune18 <lowellashton@gmail.com>
2022-12-24 13:41:07 -0800

    Fixed some typos.

--- installer/install.sh
+++ installer/install.sh
@@ -28,14 +28,14 @@ mkdir ~/.local/bin/ally
 install="$HOME/.local/bin/ally"
 
 echo "Downloading script files to install folder"
-curl -LJO https://github.com/Eyesonjune18/Ally/blob/f2c3ec3aac5da88a9ca015717ae7862220546943/src/mkal.sh -o "$install/mkal.sh"
-curl -LJO https://github.com/Eyesonjune18/Ally/blob/f2c3ec3aac5da88a9ca015717ae7862220546943/src/scral.sh -o "$install/scral.sh"
-curl -LJO https://github.com/Eyesonjune18/Ally/blob/f2c3ec3aac5da88a9ca015717ae7862220546943/src/real.sh -o "$install/real.sh"
-curl -LJO https://github.com/Eyesonjune18/Ally/blob/f2c3ec3aac5da88a9ca015717ae7862220546943/src/dual.sh -o "$install/dual.sh"
-curl -LJO https://github.com/Eyesonjune18/Ally/blob/f2c3ec3aac5da88a9ca015717ae7862220546943/src/rebash.sh -o "$install/rebash.sh"
+curl -LJO https://raw.githubusercontent.com/Eyesonjune18/Ally/main/src/mkal.sh -o "$install/mkal.sh"
+curl -LJO https://raw.githubusercontent.com/Eyesonjune18/Ally/main/src/scral.sh -o "$install/scral.sh"
+curl -LJO https://raw.githubusercontent.com/Eyesonjune18/Ally/main/src/real.sh -o "$install/real.sh"
+curl -LJO https://raw.githubusercontent.com/Eyesonjune18/Ally/main/src/dual.sh -o "$install/dual.sh"
+curl -LJO https://raw.githubusercontent.com/Eyesonjune18/Ally/main/src/rebash.sh -o "$install/rebash.sh"
 
 echo "Creating alias configuration files"
-curl -LJO https://github.com/Eyesonjune18/Ally/blob/995dc0b1eba354ff331a5a03b49b2479ce4bb883/installer/.bash_aliases_persistent -o "$HOME/.bash_aliases_persistent"
+curl -LJO https://raw.githubusercontent.com/Eyesonjune18/Ally/main/installer/.bash_aliases_persistent -o "$HOME/.bash_aliases_persistent"
 cp "$HOME/.bash_aliases_persistent" "$HOME/.bash_aliases"
 
 echo "Ally has been installed successfully. Please restart your terminal or type \"source ~/.bashrc\" to apply changes."
