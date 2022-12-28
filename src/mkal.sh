7b05618557e095ee7a5929cf1e99d1e838821536
Eyesonjune18 <lowellashton@gmail.com>
2022-12-28 14:15:03 -0800

    Added mkal extension remover.

--- src/mkal.sh
+++ src/mkal.sh
@@ -3,15 +3,27 @@ shopt -s expand_aliases
 
 # Takes a user-supplied executable name (with extension if applicable) and, if the file exists,
 # a permanent alias is created for it so it can be used with just its name rather than with a leading "./"
+# The alias will be the name of the executable with its extension removed
 
 workingdir=$(pwd)
 
+# Make sure an executable name was provided
+if [ $# -eq 0 ]
+then
+    echo "Usage: mkal <executable filename>"
+    exit 1
+fi
+
+# Set the filename variable to the first argument
 exename=$1
 withdotslash="./$exename"
 
+# Make a friendly name for the alias by removing the fil extension
+noextension="${exename%.*}"
+
 if test -f $exename; then
-        echo "Creating alias $exename for executable $exename"
-        echo alias $exename="$workingdir/$withdotslash" >> ~/.bash_aliases
+        echo "Creating alias $noextension for executable $exename"
+        echo alias $noextension="$workingdir/$withdotslash" >> ~/.bash_aliases
         echo "Alias successfully created."
 	# Reload bash to apply aliases
         rebash
