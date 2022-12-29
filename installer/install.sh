1fc1b724855ff10563bd3c6d2e52192a3f4c40f8
Eyesonjune18 <lowellashton@gmail.com>
2022-12-29 09:00:52 -0800

    Removed scral from installer files.

--- installer/install.sh
+++ installer/install.sh
@@ -29,14 +29,12 @@ install="$HOME/.local/bin/ally"
 
 echo "Downloading script files to install folder"
 curl -sLo "$install/mkal.sh" https://raw.githubusercontent.com/Eyesonjune18/Ally/main/src/mkal.sh
-curl -sLo "$install/scral.sh" https://raw.githubusercontent.com/Eyesonjune18/Ally/main/src/scral.sh
 curl -sLo "$install/real.sh" https://raw.githubusercontent.com/Eyesonjune18/Ally/main/src/real.sh
 curl -sLo "$install/dual.sh" https://raw.githubusercontent.com/Eyesonjune18/Ally/main/src/dual.sh
 curl -sLo "$install/rebash.sh" https://raw.githubusercontent.com/Eyesonjune18/Ally/main/src/rebash.sh
 
 echo "Setting up permissions for script files"
 chmod +x "$install/mkal.sh"
-chmod +x "$install/scral.sh"
 chmod +x "$install/real.sh"
 chmod +x "$install/dual.sh"
 chmod +x "$install/rebash.sh"
