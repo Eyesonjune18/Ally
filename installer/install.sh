72474668004d95d27b52a20ee0038978a7bce24e
Lowell Thoerner <lowellashton@gmail.com>
2023-01-05 10:32:50 -0800

    Changed links to reflect lowercase repo name.

--- installer/install.sh
+++ installer/install.sh
@@ -28,10 +28,10 @@ mkdir ~/.local/bin/ally
 install="$HOME/.local/bin/ally"
 
 echo "Downloading script files to install folder"
-curl -sLo "$install/mkal.sh" https://raw.githubusercontent.com/Eyesonjune18/Ally/main/src/mkal.sh
-curl -sLo "$install/real.sh" https://raw.githubusercontent.com/Eyesonjune18/Ally/main/src/real.sh
-curl -sLo "$install/dual.sh" https://raw.githubusercontent.com/Eyesonjune18/Ally/main/src/dual.sh
-curl -sLo "$install/rebash.sh" https://raw.githubusercontent.com/Eyesonjune18/Ally/main/src/rebash.sh
+curl -sLo "$install/mkal.sh" https://raw.githubusercontent.com/Eyesonjune18/ally/main/src/mkal.sh
+curl -sLo "$install/real.sh" https://raw.githubusercontent.com/Eyesonjune18/ally/main/src/real.sh
+curl -sLo "$install/dual.sh" https://raw.githubusercontent.com/Eyesonjune18/ally/main/src/dual.sh
+curl -sLo "$install/rebash.sh" https://raw.githubusercontent.com/Eyesonjune18/ally/main/src/rebash.sh
 
 echo "Setting up permissions for script files"
 chmod +x "$install/mkal.sh"
@@ -40,7 +40,7 @@ chmod +x "$install/dual.sh"
 chmod +x "$install/rebash.sh"
 
 echo "Creating alias configuration files"
-curl -sLo "$HOME/.bash_aliases_persistent" https://raw.githubusercontent.com/Eyesonjune18/Ally/main/installer/.bash_aliases_persistent
+curl -sLo "$HOME/.bash_aliases_persistent" https://raw.githubusercontent.com/Eyesonjune18/ally/main/installer/.bash_aliases_persistent
 cp "$HOME/.bash_aliases_persistent" "$HOME/.bash_aliases"
 
 echo "Ally has been installed successfully. Please restart your terminal or type \"source ~/.bashrc\" to apply changes."
